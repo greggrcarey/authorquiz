5b84de9dbb33832ae27e3ec13f9717eaeaf35fe8
gcarey <gregg.carey@outlook.com>
2022-10-17 14:47:49 -0400

    add demonstration test

--- src/AuthorQuiz.test.js
+++ src/AuthorQuiz.test.js
@@ -1,8 +1,10 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import AuthorQuiz from "./AuthorQuiz";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("Author Quiz", () => {
+  it("renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<AuthorQuiz />, div);
+  });
 });
